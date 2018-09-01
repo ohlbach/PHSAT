6bd681c08169cbcb7b95261f5a5077cad371fda9
Ohlbach <ohlbach@ifi.lmu.de>
2018-09-01 10:49:43 +0200

    SAT

--- src/test/java/Datastructures/Theory/ImplicationGraphTest.java
+++ src/test/java/Datastructures/Theory/ImplicationGraphTest.java
@@ -2,6 +2,8 @@ package Datastructures.Theory;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -104,6 +106,84 @@ public class ImplicationGraphTest {
         assertEquals("[-2, 1]",ig.addClause(-4,-2).toString());
         assertNull(ig.getImplicants(1));
         assertNull(ig.getImplicants(-1));
+    }
+    @Test
+    public void addClause9() throws Exception {
+        System.out.println("large numbers");
+        ImplicationGraph ig = new ImplicationGraph(50000);
+        assertNull(ig.addClause(1000,2000));
+        assertNull(ig.addClause(-2000,3000));
+        assertNull(ig.addClause(-2000,4000));
+        assertEquals("[1000, -2000]",ig.addClause(-4000,-2000).toString());
+        assertNull(ig.getImplicants(1000));
+        assertNull(ig.getImplicants(-1000));
+    }
+
+    @Test
+    public void addClause10() throws Exception {
+        System.out.println("back");
+        ImplicationGraph ig = new ImplicationGraph(10);
+        assertNull(ig.addClause(1,3));
+        assertNull(ig.addClause(2,3));
+        assertNull(ig.addClause(-3,4));
+        assertEquals("[3, 4]",ig.getImplicants(-1).toString());
+        assertEquals("[3, 4]",ig.getImplicants(-2).toString());
+        assertEquals("[4]",ig.getImplicants(3).toString());
+        assertEquals("[1, 2]",ig.getImplicants(-3).toString());
+        assertEquals("[-3, 1, 2]",ig.getImplicants(-4).toString());
+    }
+
+    @Test
+    public void addClause11() throws Exception {
+        System.out.println("back unit 1");
+        ImplicationGraph ig = new ImplicationGraph(10);
+        assertNull(ig.addClause(1,3));
+        assertNull(ig.addClause(2,3));
+        assertNull(ig.addClause(-3,4));
+        assertNull(ig.addClause(-4,5));
+        assertNull(ig.addClause(-1,6));
+        assertEquals("[1, 6]",ig.addClause(-4,1).toString());
+
+        assertEquals("[3, 4, 5]",ig.getImplicants(-2).toString());
+        assertNull(ig.getImplicants(1));
+        assertNull(ig.getImplicants(-1));
+        assertNull(ig.getImplicants(6));
+        assertNull(ig.getImplicants(-6));
+        assertEquals("[-3, 2]",ig.getImplicants(-4).toString());
+    }
+
+    @Test
+    public void addClause12() throws Exception {
+        System.out.println("back unit 2");
+        ImplicationGraph ig = new ImplicationGraph(10);
+        assertNull(ig.addClause(-1,2));
+        assertNull(ig.addClause(-3,4));
+        assertNull(ig.addClause(-2,5));
+        assertNull(ig.addClause(-4,5));
+        assertNull(ig.addClause(-6,7));
+        assertNull(ig.addClause(-7,8));
+        assertNull(ig.addClause(-7,-1));
+        assertNull(ig.addClause(-8,-3));
+        assertNull(ig.addClause(1,9));
+        assertEquals("[-1, -3, 9]",ig.addClause(-5,6).toString());
+        assertEquals("[5, 6, 7, 8]",ig.getImplicants(2).toString());
+        assertNull(ig.getImplicants(1));
+        assertNull(ig.getImplicants(-1));
+        assertNull(ig.getImplicants(3));
+        assertNull(ig.getImplicants(-3));
+    }
+
+    @Test
+    public void removeLiteral() throws Exception {
+        System.out.println("remove literal");
+        ImplicationGraph ig = new ImplicationGraph(10);
+        ig.addClause(-1, 2);
+        ig.addClause(-2, 3);
+        ig.addClause(-3, 4);
+        ig.addClause(-4, 5);
+        assertEquals("[2, 3, 4, 5]",ig.getImplicants(1).toString());
+        ig.removeLiteral(3);
+        assertEquals("[2, 4, 5]",ig.getImplicants(1).toString());
     }
 
 }
