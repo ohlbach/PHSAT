2a7ed05d4cc87ac20ed96776e8013354d61806c9
ohlbach <ohlbach@ifi.lmu.de>
2018-09-27 09:13:37 +0200

    SAT

--- src/main/java/Datastructures/Theory/ImplicationDAG.java
+++ src/main/java/Datastructures/Theory/ImplicationDAG.java
@@ -1,6 +1,7 @@
 package Datastructures.Theory;
 
 import Datastructures.Symboltable;
+import com.sun.istack.internal.Pool;
 
 import java.util.ArrayList;
 import java.util.TreeMap;
@@ -14,15 +15,19 @@ import java.util.function.Consumer;
  * Created by ohlbach on 26.09.2018.
  */
 public class ImplicationDAG {
-    public TreeSet<ImplicationNode> roots = new TreeSet<>();
+    private TreeSet<ImplicationNode> roots = new TreeSet<>();
     private TreeMap<Integer,ImplicationNode> nodesMap = new TreeMap<>();
-    public ArrayList<Consumer<Integer>> trueLiteralObservers = new ArrayList<>();
-    public ArrayList<BiConsumer<Integer,Integer>> implicationObservers = new ArrayList<>();
-    public ArrayList<Consumer<int[]>> equivalenceObservers = new ArrayList<>();
+    private ArrayList<Consumer<Integer>> trueLiteralObservers = new ArrayList<>();
+    private ArrayList<BiConsumer<Integer,Integer>> implicationObservers = new ArrayList<>();
+    private ArrayList<Consumer<int[]>> equivalenceObservers = new ArrayList<>();
     private int timestamp = 0;
     private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
     private final Lock readLock = rwl.readLock();
     private final Lock writeLock = rwl.writeLock();
+
+    public synchronized void addTrueLiteralObserver(Consumer<Integer> observer) {trueLiteralObservers.add(observer);}
+    public synchronized void addImplicationObserver(BiConsumer<Integer,Integer> observer) {implicationObservers.add(observer);}
+    public synchronized void addEquivalenceObserver(Consumer<int[]> observer) {equivalenceObservers.add(observer);}
 
 
     public boolean implies(Integer from, Integer to) {
@@ -80,9 +85,10 @@ public class ImplicationDAG {
         writeLock.lock();
         try{ImplicationNode fromNode = getNode(from);
             ImplicationNode toNode   = getNode(to);
+            ++timestamp;
+            if(implies(fromNode,toNode)) {return;}
+            ++timestamp;
             if(implies(toNode,fromNode)) {newEquivalence(toNode,fromNode); return;}
-            if(fromNode == null) {fromNode = new ImplicationNode(from); nodesMap.put(from,fromNode);}
-            if(toNode   == null) {toNode = new ImplicationNode(to); nodesMap.put(to,toNode);}
             int status = fromNode.addDownNode(toNode);
             if(status == 1) {return;}
             if(status == -1) {newTrueLiteral(-from);}
@@ -149,24 +155,48 @@ public class ImplicationDAG {
         for(ImplicationNode node : downNodes) {if(!downs.contains(node)) {downs.add(node);}}}
 
 
-    public void apply(Integer literal, Consumer<Integer> consumer) {
-        readLock.lock();
+    public void apply(Integer literal, boolean down, Consumer<Integer> consumer) {
+        writeLock.lock();
+        ++timestamp;
         try{ImplicationNode node = nodesMap.get(literal);
-            if(node != null) {++timestamp; apply(node,consumer);}
+            if(node != null) {++timestamp; apply(node,down,consumer);}
             else {consumer.accept(literal);}}
-        finally{readLock.unlock();}}
+        finally{writeLock.unlock();}}
 
-    public void apply(ImplicationNode node, Consumer<Integer> consumer) {
+    private void apply(ImplicationNode node, boolean down, Consumer<Integer> consumer) {
+        consumer.accept(node.literal);
+        ArrayList<ImplicationNode> nodes = down ? node.downNodes : node.upNodes;
+        if(nodes != null) {
+            for(ImplicationNode nextNode : nodes) {
+                if(nextNode.timestamp != timestamp) {
+                    nextNode.timestamp = timestamp;
+                    apply(nextNode,down,consumer);}}}}
+
+    public void applyToRoots(Consumer<Integer>consumer){
         readLock.lock();
-        try{consumer.accept(node.literal);
-            ArrayList<ImplicationNode> downNodes = node.downNodes;
-            if(downNodes != null) {
-                for(ImplicationNode downNode : downNodes) {
-                    if(downNode.timestamp != timestamp) {
-                        downNode.timestamp = timestamp;
-                        apply(downNode,consumer);}}}}
+        try{for(ImplicationNode root : roots) {consumer.accept(root.literal);}}
         finally{readLock.unlock();}}
 
+    public void applyToImplications(Integer from, Integer to, BiConsumer<Integer,Integer> consumer) {
+        readLock.lock();
+        try {ImplicationNode fromNode = getNode(from);
+             ImplicationNode toNode = getNode(to);
+             applyToImplications(fromNode,toNode,consumer);}
+        finally{readLock.unlock();}}
+
+
+    private void applyToImplications(ImplicationNode from, ImplicationNode to, BiConsumer<Integer,Integer> consumer) {
+        consumer.accept(from.literal,to.literal);
+        ArrayList<ImplicationNode> upNodes   = from.upNodes;
+        ArrayList<ImplicationNode> downNodes = to.upNodes;
+        if(upNodes == null) {
+            if(downNodes == null) {return;}
+            for(ImplicationNode downNode : downNodes) {applyToImplications(from,downNode,consumer);}}
+        else {
+            for(ImplicationNode upNode : upNodes) {consumer.accept(upNode.literal,to.literal);}
+            if(downNodes == null) {return;}
+            for(ImplicationNode upNode : upNodes) {
+                for(ImplicationNode downNode : downNodes) {applyToImplications(upNode,downNode,consumer);}}}}
 
 
     private void removeFalseLiteral(Integer falseLiteral) {
@@ -190,18 +220,19 @@ public class ImplicationDAG {
 
 
     public String toString(Symboltable symboltable) {
-        StringBuilder st = new StringBuilder();
-        for(ImplicationNode node : roots) {
-            toString(node,symboltable,1,st);
-            st.append("\n");}
+        readLock.lock();
+        try{StringBuilder st = new StringBuilder();
+            for(ImplicationNode node : roots) {
+                toString(node,symboltable,0,st);}
         return st.toString();}
+        finally{readLock.unlock();}}
 
     private void toString(ImplicationNode node, Symboltable symboltable, int indent, StringBuilder st) {
-        st.append(String.format("%"+indent+"s"," ")).append(node.toString()).append( " -> ");
-        if(node.isAlmostLeafNode()) {
-            for(int i = 0; i < node.downNodes.size()-1; ++i) {st.append(node.downNodes.get(i).toString()).append(",");}
-            st.append(node.downNodes.get(node.downNodes.size()-1).toString());}
-        else {st.append("\n"); toString(node,symboltable,indent+3,st);}}
-
+        if(node.downNodes == null) {return;}
+        if(indent > 0) {st.append(String.format("%"+indent+"s"," "));}
+        st.append(node.toString()).append( " -> ");
+        for(int i = 0; i < node.downNodes.size()-1; ++i) {st.append(node.downNodes.get(i).toString()).append(",");}
+        st.append(node.downNodes.get(node.downNodes.size()-1).toString()).append("\n");
+        for(ImplicationNode downNode : node.downNodes) {toString(downNode,symboltable,indent+5,st);}}
 
 }
--- src/test/java/Datastructures/Theory/ImplicationDAGTest.java
+++ src/test/java/Datastructures/Theory/ImplicationDAGTest.java
@@ -14,7 +14,29 @@ public class ImplicationDAGTest {
     public void addImplication() throws Exception {
         ImplicationDAG id = new ImplicationDAG();
         id.addImplication(1,2);
-
+        assertEquals("1 -> 2\n",id.toString());
+        id.addImplication(1,3);
+        assertEquals("1 -> 2,3\n",id.toString());
+        id.addImplication(2,4);
+        assertEquals("1 -> 2,3\n" +
+                "     2 -> 4\n",id.toString());
+        id.addImplication(3,4);
+        assertEquals("1 -> 2,3\n" +
+                "     2 -> 4\n" +
+                "     3 -> 4\n",id.toString());
+        id.addImplication(4,5);
+        assertEquals("1 -> 2,3\n" +
+                "     2 -> 4\n" +
+                "          4 -> 5\n" +
+                "     3 -> 4\n" +
+                "          4 -> 5\n",id.toString());
+        System.out.printf("I " + id.implies(1,5));
+        id.addImplication(1,5);
+        assertEquals("1 -> 2,3\n" +
+                "     2 -> 4\n" +
+                "          4 -> 5\n" +
+                "     3 -> 4\n" +
+                "          4 -> 5\n",id.toString());
         System.out.println(id.toString());
 
     }
