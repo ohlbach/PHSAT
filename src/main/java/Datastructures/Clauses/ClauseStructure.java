0ca736277b06704f7f1fabfe5099b355de11e209
ohlbach <ohlbach@ifi.lmu.de>
2018-12-06 20:34:30 +0100

    SAT

--- src/main/java/Datastructures/Clauses/ClauseStructure.java
+++ src/main/java/Datastructures/Clauses/ClauseStructure.java
@@ -1,8 +1,11 @@
 package Datastructures.Clauses;
 
-/**
+/** These tags are used for clauses and clause sets
  * Created by ohlbach on 03.12.2018.
  */
 public enum ClauseStructure {
-    POSITIVE, NEGATIVE, MIXED, BOTH;
+    POSITIVE, // clause: it consists only positive literals. ClauseList: it contains no negative clauses
+    NEGATIVE, // clause: it consists only negative literals. ClauseList: it contains no positive clauses
+    BOTH,     // clause list: it contains only mixed literals
+    MIXED;    // clause: it contains positive and negative literals. ClauseList: no restriction.
 }
